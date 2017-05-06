d8525b2ba3ed98f5e50d4a9e281cf1a51470954b
Conor OShea <osheac5@tcd.ie>
2017-05-06 10:20:43 +0100

    db

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -49,8 +49,24 @@ public class Main {
   }
 
   @RequestMapping("/")
-  String index() {
-    return "index";
+  String index(Map<String, Object> model) {
+    try (Connection connection = dataSource.getConnection()) {
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+      ArrayList<String> output = new ArrayList<String>();
+      while (rs.next()) {
+        output.add("Read from DB: " + rs.getTimestamp("tick"));
+      }
+
+      model.put("records", output);
+      return "index";
+    } catch (Exception e) {
+      model.put("message", e.getMessage());
+      return "error";
+    }
   }
 
 
