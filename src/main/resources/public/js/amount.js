cdfe959dff72ec38bd723bbb2ec8a614a81cbc61
Conor OShea <osheac5@tcd.ie>
2017-08-12 21:35:08 +0100

    final ui touches

--- src/main/resources/public/js/amount.js
+++ src/main/resources/public/js/amount.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  debugger;
-  console.log("hello!");
   $date = $('.datepicker');
   $date.pickadate();
-  $date.val($.datepicker.formatDate('dd/mm/yyyy', new Date()));
+  $date.val($.datepicker.formatDate('dd/mm/yy', new Date()));
 })
